2efd2e7bc6e0c879f6647d5967fb2be29c463dd0
danbari93@gmail.com <danbari93@gmail.com>
2015-11-11 14:21:43 -0600

    page title

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,17 @@
 class HomeController < ApplicationController
   def index
+  	@title = "Home | Pacific Coffee"
   end
 
   def search
+  	@title = "Search | Pacific Coffee"
   end
 
   def about
+  	@title = "About | Pacific Coffee"
   end
 
   def contact
+  	@title = "Contact | Pacific Coffee"
   end
 end
